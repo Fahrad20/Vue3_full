6436ca7a61870d4a9cf0599083863090b9a8cf6c
Fahrad Levonyan <Fahrad_Home>
2023-09-16 19:06:39 +0400

    full vue app

---
+++ src/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>
+      Данное приложение было создано в результате обучения на канале Ulbi TV
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
